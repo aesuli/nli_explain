a10f87ea66be97f2e7c6ed536d4869b0fa2051f4
Andrea Esuli <andrea@esuli.it>
2022-09-19 12:29:37 +0200

    memory use fix

--- index.py
+++ index.py
@@ -1,30 +1,26 @@
 import os
 import pickle
-from collections import defaultdict
 
 from tqdm.auto import tqdm
 
 from custom_tokenization import feature_types
 
 
-def index(dataset):
-    print('indexing', dataset)
+def index(dataset, feature_type):
+    print('indexing', dataset, feature_type)
     with open(os.path.join('data', dataset + '.pkl'), mode='rb') as inputfile:
         X = pickle.load(inputfile)
         y = pickle.load(inputfile)
 
-        print(len(X),len(y))
+        print(len(X), len(y))
 
     from custom_tokenization import spacy_tokenizer
 
-    for feature_type in feature_types:
-        print(f'\t{feature_type}')
-        X_indexed = list()
-        for text in tqdm(X):
-            X_indexed.append(spacy_tokenizer(text, feature_type))
-        with open(os.path.join('data', dataset + '_indexed_' + feature_type + '.pkl'), mode='wb') as outputfile:
-            pickle.dump(X_indexed, outputfile)
-    print('indexed', dataset)
+    X_indexed = list()
+    for text in tqdm(X):
+        X_indexed.append(spacy_tokenizer(text, feature_type))
+    with open(os.path.join('data', dataset + '_indexed_' + feature_type + '.pkl'), mode='wb') as outputfile:
+        pickle.dump(X_indexed, outputfile)
 
 
 if __name__ == '__main__':
@@ -38,4 +34,5 @@ if __name__ == '__main__':
         'EFCAMDAT2_L2',
         'EFCAMDAT2_L3'
     ]:
-        index(dataset)
+        for feature_type in feature_types:
+            index(dataset, feature_type)
